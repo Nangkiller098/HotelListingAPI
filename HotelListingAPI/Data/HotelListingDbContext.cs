a8e4279c17f6e0476e0210d9f6a18d500c22b435
hengsomnang <hengsomnang@SOMNANG-LAPTOP>
2023-05-24 16:03:57 +0700

    Add project files.

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> countries { get; set; }
+        //protected override void ConfigureConventions(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    //modelBuilder.Entity<Country>().HasData(
+        //    //    new Country
+        //    //    {
+        //    //        Id = 1,
+        //    //        Name = "test",
+        //    //        ShortName = "t",
+        //    //    },
+        //    //    new Country
+        //    //    {
+        //    //        Id=2,
+        //    //        Name="test2",
+        //    //        ShortName="t2"
+
+        //    //    }
+        //    //    );
+        //}
+    }
+}
