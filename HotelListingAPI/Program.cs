c1c78f7e443280939cf1fbac25f70d0e8749ccda
hengsomnang <hengsomnang@SOMNANG-LAPTOP>
2023-05-25 15:23:56 +0700

    master

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -36,6 +36,7 @@ builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountiesRepository, CountriesRepository>();
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
 
 
 var app = builder.Build();
